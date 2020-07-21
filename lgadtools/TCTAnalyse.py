015d3281b7edfc538aa72e41eb40de01b22260a1
Matias Senger <m.senger@hotmail.com>
2020-07-21 15:25:59 +0200

    Added properties x0, y0, z0

--- lgadtools/TCTAnalyse.py
+++ lgadtools/TCTAnalyse.py
@@ -10,7 +10,7 @@ class PSTCT:
 	
 	def GetWF(self, ch=0, x=0, y=0, z=0, u1=0, u2=0):
 		# Returns an LGADSignal object.
-		# Arguments follow the same structure as GetHA described in particulars.si/TCTAnalyse/TCTAnalyse-UserGuide.html
+		# Arguments follow the same structure as "GetHA" described in particulars.si/TCTAnalyse/TCTAnalyse-UserGuide.html
 		if type(ch) != type(x) != type(y) != type(z) != type(u1) != type(u2) != int:
 			raise TypeError('All arguments must be int numbers')
 		N_samples = self.root_object.GetHA(ch,x,y,z,u1,u2).GetNbinsX()
@@ -20,14 +20,17 @@ class PSTCT:
 	
 	@property
 	def Nx(self):
+		# Number of "x" steps
 		return self.root_object.Nx
 	
 	@property
 	def Ny(self):
+		# Number of "y" steps
 		return self.root_object.Ny
 	
 	@property
 	def Nz(self):
+		# Number of "z" steps
 		return self.root_object.Nz
 	
 	@property
@@ -36,12 +39,27 @@ class PSTCT:
 	
 	@property
 	def dx(self):
+		# Size of one "x" step in meters
 		return self.root_object.dx*2e-6
 		
 	@property
 	def dy(self):
+		# Size of one "y" step in meters
 		return self.root_object.dy*2e-6
 
 	@property
 	def dz(self):
+		# Size of one "z" step in meters
 		return self.root_object.dz*2e-6
+	
+	@property
+	def x0(self):
+		return self.root_object.x0*2e-6
+	
+	@property
+	def y0(self):
+		return self.root_object.y0*2e-6
+	
+	@property
+	def z0(self):
+		return self.root_object.z0*2e-6
